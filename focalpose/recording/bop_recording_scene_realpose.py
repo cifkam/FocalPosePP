55515cd1780aee0b33b1147f42d34e8e2c5830a2
Martin Cífka <1591124+cifkam@users.noreply.github.com>
2022-06-25 23:41:50 +0200

    Added bop_recording_scene_realpose

---
+++ focalpose/recording/bop_recording_scene_realpose.py
@@ -0,0 +1,68 @@
+import numpy as np
+import numpy.random as nr
+import pinocchio as pin
+
+from focalpose.simulator import Camera
+from focalpose.recording.bop_recording_scene import BopRecordingScene
+
+class BopRecordingSceneRealPose(BopRecordingScene):
+    def __init__(self,
+
+                 real_dataset,
+
+                 urdf_ds='ycbv',
+                 texture_ds='shapenet',
+                 domain_randomization=True,
+                 textures_on_objects=False,
+                 n_objects_interval=(1, 1),
+                 #objects_xyz_interval=((0.0, -0.5, -0.15), (1.0, 0.5, 0.15)),
+                 proba_falling=0.0,
+                 resolution=(640, 480),
+                 #focal_interval=(515, 515),
+                 #camera_distance_interval=(0.5, 1.5),
+                 border_check=True,
+                 gpu_renderer=True,
+                 n_textures_cache=50,
+                 seed=0):
+
+        super().__init__(
+            urdf_ds=urdf_ds,
+            texture_ds=texture_ds,
+            domain_randomization=domain_randomization,
+            textures_on_objects=textures_on_objects,
+            n_objects_interval=n_objects_interval,
+            #objects_xyz_interval=objects_xyz_interval,
+            proba_falling=proba_falling,
+            resolution=resolution,
+            #focal_interval=focal_interval,
+            #camera_distance_interval=camera_distance_interval,
+            border_check=border_check,
+            gpu_renderer=gpu_renderer,
+            n_textures_cache=n_textures_cache,
+            seed=seed)
+
+        self.real_dataset = real_dataset
+
+    def sample_camera(self):
+        i = nr.randint(0, len(self.real_dataset))
+        f = self.real_dataset.f[i]
+        TWC = self.real_dataset.TWC[i]
+
+        K = np.zeros((3, 3), dtype=np.float)
+        W, H = max(self.resolution), min(self.resolution)
+        K[0, 0] = f
+        K[1, 1] = f
+        K[0, 2] = W / 2
+        K[1, 2] = H / 2
+        K[2, 2] = 1.0
+        cam = Camera(resolution=self.resolution, client_id=self._client_id)
+        cam.set_intrinsic_K(K)
+        cam.set_extrinsic_T(TWC)
+        return cam
+        
+    def objects_pos_orn_rand(self):
+        self.hide_plane()
+        for body in self.bodies:
+            pos = np.zeros(3)
+            orn = pin.Quaternion().coeffs()
+            body.pose = pos, orn
