87fad83f9dcc144a6a2c2e20813811df56b8047b
Martin Cífka <1591124+cifkam@users.noreply.github.com>
2023-09-15 19:08:01 +0200

    Replaced invalid links

--- download_data.sh
+++ download_data.sh
@@ -1,9 +1,13 @@
 mkdir -p ./local_data/ && cd ./local_data/
 
 wget http://pix3d.csail.mit.edu/data/pix3d.zip
-wget http://ai.stanford.edu/~jkrause/car196/cars_train.tgz
-wget http://ai.stanford.edu/~jkrause/car196/cars_test.tgz
-wget http://shapenet.cs.stanford.edu/shapenet/obj-zip/ShapeNetCore.v1/02958343.zip
+#wget http://ai.stanford.edu/~jkrause/car196/cars_train.tgz
+wget https://data.ciirc.cvut.cz/public/projects/2023FocalPosePP/StanfordCars/cars_train.tgz
+#wget http://ai.stanford.edu/~jkrause/car196/cars_test.tgz
+wget https://data.ciirc.cvut.cz/public/projects/2023FocalPosePP/StanfordCars/cars_test.tgz
+#wget http://shapenet.cs.stanford.edu/shapenet/obj-zip/ShapeNetCore.v1/02958343.zip
+wget https://data.ciirc.cvut.cz/public/projects/2023FocalPosePP/ShapeNetCore.v1/02958343.zip
+
 wget https://dl.fbaipublicfiles.com/dino/dino_vitbase8_pretrain/dino_vitbase8_pretrain.pth
 wget http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar
 git clone https://github.com/yangyi02/finegrained-pose.git
